8cc5e0ce9a0e3c4ed35d36e4cd34f653b4b84b37
rray-h <idamsword@gmail.com>
2022-12-21 16:27:13 +0300

    Страница фриласнера

--- WebApplication1/wwwroot/js/site.js
+++ WebApplication1/wwwroot/js/site.js
@@ -1,4 +1,22 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿let modal = document.querySelector('.user__modal');
+let btnList = document.querySelectorAll('.user__btn');
+let close = document.querySelectorAll('.user__button');
 
-// Write your JavaScript code.
+
+for(let i = 0; i < btnList.length; i++) {
+    if(btnList[i].querySelector = '.user__btn') {
+        btnList[i].addEventListener('click', function(){
+            modal.classList.add('block')
+        })          
+    }
+}
+
+for(let i = 0; i < close.length; i++) {
+    if(close[i].querySelector = '.user__button') {
+        close[i].addEventListener('click', function(){
+            modal.classList.remove('block')
+        })
+    }
+}
+
+
